25ff4d190fe07f592c28d5b43f31653485e28c9e
bonnerjallah <121054718+bonnerjallah@users.noreply.github.com>
2024-09-13 18:50:37 -0400

    modified socket.io url

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -1,7 +1,9 @@
 // socket.js
 import { io } from "socket.io-client";
+const frontendURL = import.meta.env.VITE_REACT_APP_FRONTEND_URL
 
-const socket = io("http://localhost:3001", {
+
+const socket = io(frontendURL, {
     transports: ["websocket"],
     reconnectionAttempts: 5
 });
